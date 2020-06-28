623950145f76de2650ed6b4d34a4e18747f4036b
Kurata Sayuri <MagicalSayuri@gmail.com>
2020-06-28 23:00:29 +0900

    use utf-8 with bom for msvc.

--- test/chrono-test.cc
+++ test/chrono-test.cc
@@ -1,4 +1,4 @@
-// Formatting library for C++ - time formatting tests
+﻿// Formatting library for C++ - time formatting tests
 //
 // Copyright (c) 2012 - present, Victor Zverovich
 // All rights reserved.
